b9eba23b6261b7f9b9f2b5dfbaab6bee36d5be3e
lilinyan1 <lilinyan1@hotmail.com>
2017-03-06 00:23:47 -0500

    Added 2 views,  2 activities and gMap integration
    Created the MainView, MainActivity, PropertyVIew, PropertyActivity;
    Added the GoogleMap Integration;

--- StudentHousingApp/StudentHousing/MainActivity.cs
+++ StudentHousingApp/StudentHousing/MainActivity.cs
@@ -3,52 +3,98 @@ using Android.Widget;
 using Android.OS;
 using System;
 using StudentHousing.Dto;
+using Android.Gms.Maps;
+using Android.Support.V4.App;
+using Android.Gms.Maps.Model;
+using Android.Locations;
+using System.Collections.Generic;
+using Android.Content;
+using System.Linq;
 
 namespace StudentHousing
 {
 	[Activity(Label = "Student Housing", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+	public class MainActivity : FragmentActivity, IOnMapReadyCallback
 	{
-		//int count = 1;
+        GoogleMap _googleMap;
+        Location _currentLocation;
+        LocationManager _locationManager;
+        string _locationProvider;
 
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button btnRT = FindViewById<Button>(Resource.Id.btnTestRead);
-
-			btnRT.Click += ReadTest;
-
-		}
-
-		void ReadTest(object sender, EventArgs ea)
-		{
-			
-			TextView tv = FindViewById<TextView>(Resource.Id.testView);
-			tv.Text = "";
-
-            //var id = 1;
-
-            //var property = Property.GetByID(id);
-            var property = new PropertyDto { ID = 1, Address = "123" , Latitude = 2.1, Longitude = 3.0, Price = 500, School = "Conestoga"};
-			try
-			{
-				foreach (var prop in typeof(PropertyDto).GetFields())
-				{
-					tv.Text += " " + prop.Name + ": " + prop.GetValue(property) + "\n";
-				}
-			}
-			catch (Exception e)
-			{
+            try
+            {
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.Main);
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
-				//Logging.Log("null", "null", e.Message, false);
-			}
+            }
 
-		}
+            MapFragment mapFrag = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFrag.GetMapAsync(this);
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {        
+            googleMap.MyLocationEnabled = true;
+            _googleMap = googleMap;
+
+            googleMap.MyLocationChange += (s, e) =>
+            {               
+                if (_currentLocation == null)
+                {
+                    var latlng = new LatLng(e.Location.Latitude, e.Location.Longitude);
+                    _googleMap.MoveCamera(CameraUpdateFactory.NewLatLngZoom(latlng, 15));
+                }
+                _currentLocation = e.Location;
+            };
+
+            googleMap.InfoWindowClick += MapOnInfoWindowClick;
+
+            var properties = GetPropertiesCloseBy();
+            foreach (var property in properties)
+            {
+                googleMap.AddMarker(new MarkerOptions()
+                .SetPosition(new LatLng(property.Latitude, property.Longitude))
+                .SetTitle(property.Address)
+                .SetSnippet(string.Format("$ {0}", property.Price))).Tag = property.ID;
+            }
+        }
+
+        private void MapOnInfoWindowClick(object sender, GoogleMap.InfoWindowClickEventArgs infoWindowClickEventArgs)
+        {
+            var currentMarker = infoWindowClickEventArgs.Marker;  
+            var propertyActivity = new Intent(this, typeof(PropertyActivity));
+            propertyActivity.PutExtra("PropertyId", (int)currentMarker.Tag);
+            StartActivity(propertyActivity);
+        }
+
+        private List<PropertyDto> GetPropertiesCloseBy()
+        {
+            var properties = new List<PropertyDto>();
+            properties.Add(new PropertyDto
+            {
+                ID = 1,
+                Latitude = 43.471487,
+                Longitude = -80.599914,
+                Price = 500,
+                Address = "990 Creekside Dr, Waterloo, ON N2V 2W3"
+            });
+            properties.Add(new PropertyDto
+            {
+                ID = 2,
+                Latitude = 43.391943,
+                Longitude = -80.408000,
+                Price = 510,
+                Address = "5 Orchard Mill Crescent, Kitchener, ON N2P 1T2"
+            });        
+            return properties;
+        }
+
 	}
 }
---
+++ StudentHousingApp/StudentHousing/PropertyActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using StudentHousing.Dto;
+
+namespace StudentHousing
+{
+    [Activity(Label = "Property", Icon = "@mipmap/icon")]
+    public class PropertyActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);            
+            SetContentView(Resource.Layout.Property);
+
+            var propertyId = Intent.GetIntExtra("PropertyID", int.MinValue);
+            var property = GetProertyById(propertyId);
+            FindViewById<TextView>(Resource.Id.priceValue).Text = property.Price.ToString();
+            FindViewById<TextView>(Resource.Id.address).Text = property.Address;
+            //foreach (var prop in typeof(PropertyDto).GetFields())
+            //{
+            //    tv.Text += " " + prop.Name + ": " + prop.GetValue(property) + "\n";
+            //}
+
+        }
+
+        private PropertyDto GetProertyById(int id)
+        {
+            return new PropertyDto
+            {
+                ID = 1,
+                Latitude = 43.471487,
+                Longitude = -80.599914,
+                Price = 500,
+                Address = "990 Creekside Dr, Waterloo, ON N2V 2W3"
+            };
+        }
+        
+    }
+}
