a035fae82a6d917a699930d49c9c87d7ea0dfe97
herpaderpus <mcocca2499@conestogac.on.ca>
2017-03-08 16:38:47 -0500

    Added WebAPI project

---
+++ StudentHousingServer/WebAPI4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAPI4.Startup))]
+namespace WebAPI4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
