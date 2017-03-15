455d01d274a5fb7ddc8de15675750e59400b4c66
xzhen1446 <xzhen1446@conestogac.on.ca>
2017-03-15 16:20:01 -0400

    ttt

--- StudentHousingServer/Dto/BookmarkDto.cs
+++ StudentHousingServer/Dto/BookmarkDto.cs
@@ -3,8 +3,10 @@ namespace StudentHousing.Dto
 {
 	public class BookmarkDto
 	{
-		public BookmarkDto()
-		{
-		}
+		//
+		public int userID { get; set; } = int.MinValue;
+		public int propertyID { get; set; } = int.MinValue;
+		public DateTime bookmarkDate { get; set; } = DateTime.MinValue;
+		public string comment { get; set; } = string.Empty;
 	}
 }
