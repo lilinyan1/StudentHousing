121047da94f98397f50e5d83d68a3a32e8cdb096
herpaderpus <mcocca2499@conestogac.on.ca>
2017-04-04 20:14:01 -0400

    Added routing and a GetRating function to the controller

--- StudentHousingServer/WebApi/Controllers/PropertyController.cs
+++ StudentHousingServer/WebApi/Controllers/PropertyController.cs
@@ -9,25 +9,34 @@ using System.Web.UI;
 
 namespace WebApi.Controllers
 {
+    [RoutePrefix("api/property")]
     public class PropertyController : ApiController
     {
         // GET: api/Db
+        [Route("")]
         public IEnumerable<string> Get()
         {
             return new string[] { "person1", "person2" };
         }
 
         // GET: api/Db/5
+        [Route("{id}")]
         public Property Get(int id)
         {
             return Property.GetByID(id);
         }
-
+        [Route("{lat}/{lng}")]
         public List<Property> Get(string lat, string lng)
         {
             var dLat = Convert.ToDouble(lat.Replace('e', '.'));
             var dLng = Convert.ToDouble(lng.Replace('e', '.'));
             return Property.SearchCloseByProperties(dLat, dLng);
+        }
+
+        [Route("rating/{id}")]
+        public double GetRating(int id)
+        {
+            return Property.GetRating(id);
         }
 
         // POST: api/Db
